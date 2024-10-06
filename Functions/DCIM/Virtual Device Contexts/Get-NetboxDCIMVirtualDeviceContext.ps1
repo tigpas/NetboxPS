03f1271781ef329b85652a06f36954c2fd1cc007
Pascal Liehne <git@pascal.liehne.eu>
2024-10-06 18:24:08 +0200

    add NetboxDCIMVirtualDeviceContext

---
+++ Functions/DCIM/Virtual Device Contexts/Get-NetboxDCIMVirtualDeviceContext.ps1
@@ -0,0 +1,39 @@
+﻿function Get-NetboxDCIMVirtualDeviceContext {
+    [CmdletBinding()]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [Parameter(ValueFromPipelineByPropertyName = $true)]
+        [uint64]$Id,
+
+        [string]$Name,
+
+        [object]$Status,
+
+        [uint16]$Identifier,
+
+        [string]$Device,
+
+        [uint64]$Device_Id,
+
+        [uint64]$Tenant_Id,
+
+        [string]$Tenant,
+
+        [uint16]$Limit,
+
+        [uint16]$Offset,
+
+        [switch]$Raw
+    )
+
+    process {
+        $Segments = [System.Collections.ArrayList]::new(@('dcim', 'virtual-device-contexts'))
+
+        $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters
+
+        $URI = BuildNewURI -Segments $URIComponents.Segments -Parameters $URIComponents.Parameters
+
+        InvokeNetboxRequest -URI $URI -Raw:$Raw
+    }
+}
---
+++ Functions/DCIM/Virtual Device Contexts/Set-NetboxDCIMVirtualDeviceContext.ps1
@@ -0,0 +1,45 @@
+﻿function Set-NetboxDCIMVirtualDeviceContext {
+    [CmdletBinding(ConfirmImpact = 'Medium',
+        SupportsShouldProcess = $true)]
+    [OutputType([pscustomobject])]
+    param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [uint64[]]$Id,
+
+        [string]$Name,
+
+        [object]$Status,
+
+        [uint16]$Identifier,
+
+        [uint64]$Device,
+
+        [uint64]$Primary_IP4,
+
+        [uint64]$Primary_IP6,
+
+        [uint64]$Tenant,
+
+        [string]$Comments,
+
+        [switch]$Force
+    )
+
+    process {
+        foreach ($VirtualDeviceContextId in $Id) {
+            $CurrentVirtualDeviceContext = Get-NetboxDCIMVirtualDeviceContext -Id $VirtualDeviceContextId -ErrorAction Stop
+
+            $Segments = [System.Collections.ArrayList]::new(@('dcim', 'virtual-device-contexts', $CurrentVirtualDeviceContext.Id))
+
+            $URIComponents = BuildURIComponents -URISegments $Segments.Clone() -ParametersDictionary $PSBoundParameters -SkipParameterByName 'Id'
+
+            $URI = BuildNewURI -Segments $Segments
+
+            if ($Force -or $pscmdlet.ShouldProcess("Virtual Device Context ID $($CurrentVirtualDeviceContext.Id)", "Set")) {
+                InvokeNetboxRequest -URI $URI -Body $URIComponents.Parameters -Method PATCH
+            }
+        }
+    }
+}
